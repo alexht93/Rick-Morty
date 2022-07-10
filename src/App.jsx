8a141d4ed181d01528d602f37704bf55e278c109
alexht93 <alex.h.torres@outlook.com>
2022-07-09 23:59:05 -0600

    third

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Location from './Components/Location'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import HeaderImg from './Components/HeaderImg';
 
 
 
@@ -10,9 +11,7 @@ function App() {
 
   return (
    <div className='App'>
-    <div className='containerBanner'>
-        <img src="./src/assests/image 2.png" alt="" />
-      </div>
+      <HeaderImg/>
         <div className='container mt-5'>
       <Location placeholder="Type a location Id..." />
     </div>
---
+++ src/Components/HeaderImg.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeaderImg = () => {
+    return (
+        <div>
+            <div className='containerBanner'>
+                <img src="./src/assests/image 2.png" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default HeaderImg;
