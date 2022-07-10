c27bf48b803f1b5c6191ccc9a5e68e0472e1dc80
alexht93 <alex.h.torres@outlook.com>
2022-07-10 14:38:24 -0600

    newupdate

--- src/Components/HeaderImg.jsx
+++ src/Components/HeaderImg.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import front from "../assets/front.png"
+import back from "../assets/back.png"
 
 const HeaderImg = () => {
 
 
     return (
-        <div>
-            <img src={front} alt="" />
+        <div style={{backgroundImage: `url(${back})`, backgroundSize: "100%", backgroundPositionY: "center",}}>
+            <img src={front} alt="" style={{width: "100%", maxWidth: "500px"}} />
         </div>
     );
 };
