2d403dd397a2de4f1f091f2ffaa2acaad794c57e
alexht93 <alex.h.torres@outlook.com>
2022-07-10 15:05:26 -0600

    lastlastUpdate

--- src/Components/ResidentInfo.jsx
+++ src/Components/ResidentInfo.jsx
@@ -18,7 +18,7 @@ const ResidentInfo = ({ resident }) => {
 
     return (
         <div className='col mb-4 '>
-            <div className='card' style={ {minWidth: "200px"}}>
+            <div className='card' style={ {minWidth: "100px", maxWidth: "300px"}}>
                 <img  src={residentInfo.image} alt=""/>
                 <div className='card-body status' style={{backgroundColor: "#052226"}}>
                 
@@ -26,7 +26,7 @@ const ResidentInfo = ({ resident }) => {
                     <hr />
                     <div className="status">
                     <span className={residentInfo.status}></span>
-                    <p> <b>Status:</b> {residentInfo.status} </p>
+                    <p> <b>Status:</b> {residentInfo.status}</p>
                     <p>  <b>Origin:</b> {residentInfo.origin?.name}</p>
                     <p><b>Episode appearances:</b> {residentInfo.episode?.length}</p>
                     </div>
